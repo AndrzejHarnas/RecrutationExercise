ae834b57bf801f5e3f24185393b5812eed9d60f2
Harnas_20 <Harnas_20@o2.pl>
2020-01-27 17:52:02 +0100

    conection to aws is working :):

--- aws.js
+++ aws.js
@@ -3,7 +3,7 @@ var AWS = require('aws-sdk');
 var uuid = require('uuid');
 
 var bucketName = 'lcloud-427-ts' + uuid.v4();
-var keyName = 'AKIAS464CIUF2WLQ6E7X4k63ImEjRHoWD2/B/mwZ/7vn+cd5K/+7diOXaVBP';
+var keyName = '4k63ImEjRHoWD2/B/mwZ/7vn+cd5K/+7diOXaVBP';
 
 var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
 
